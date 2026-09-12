9466ae9bfedb7ef53237e71eae51ad0b96b854f3
SALEH-AL-KHULAIFI <new.mtn.new@gmail.com>
2026-09-12 21:36:53 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,23 @@
-plugins { id("com.android.application") }
-android { namespace="com.saleh.wadhkur"; compileSdk=36
- defaultConfig { applicationId="com.saleh.wadhkur"; minSdk=23; targetSdk=35; versionCode=200; versionName="2.0.0" }
- buildTypes { release { isMinifyEnabled=false } }
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.saleh.wadhkur"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.saleh.wadhkur"
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 210
+        versionName = "2.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
 }
